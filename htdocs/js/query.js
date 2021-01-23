cae2d10f957a9dcfcc23c803191e17ab7b54d53c
Matthias <17014025@uhi.ac.uk>
2021-01-23 23:18:49 +0000

    query updates

--- htdocs/js/query.js
+++ htdocs/js/query.js
@@ -2,21 +2,34 @@ function init() {
 	document.getElementById("company1").addEventListener("keyup", autocomplete);
 	document.getElementById("company2").addEventListener("keyup", autocomplete);
 	document.getElementById("btnSubmit").addEventListener("click", compare);
-	yahooQuery('TSLA', 'UK')
+}
+let company1Info;
+let company2Info;
+function autocomplete() {}
+function compare() {
+	event.preventDefault();
+	console.log("button pushed");
+	let cp1 = document.getElementById("company1");
+	let cp2 = document.getElementById("company2");
+	yahooQuery(cp1.value, 'UK')
 	.then(data => {
+		company1Info = data;
+		console.log(data); // JSON data parsed by `data.json()` call
+	});
+	yahooQuery(cp2.value, 'UK')
+	.then(data => {
+		company2Info = data;
 		console.log(data); // JSON data parsed by `data.json()` call
 	});
 }
-function autocomplete() {}
-function compare() {}
 
 
-async function yahooQuery(symbol, region = "US") {
+async function yahooQuery(symbol, region = "US", other = {}) {
 	let url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?';
 	if(typeof symbol !== 'undefined') {
 		url+="symbol="+symbol+"&";
 	}
-	url+="region="+symbol;
+	url+="region="+region;
 
 	const response = await fetch(url, {
 		method: 'GET', // *GET, POST, PUT, DELETE, etc.
