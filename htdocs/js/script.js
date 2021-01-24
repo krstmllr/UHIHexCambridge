db7933d9070d3a81e1c16dd5783302496df58faf
Matthias <17014025@uhi.ac.uk>
2021-01-24 14:01:35 +0000

    Update script.js

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -46,26 +46,26 @@ function complete1(){
 
 function complete2(){
 	if ($("fighter2").focus()){
-			var searchTerm = document.getElementById("fighter2").value;
-			console.log(searchTerm);
-			const settings = {
-				"async": true,
-				"crossDomain": true,
-				"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q="+ searchTerm +	"&region=US",
-				"method": "GET",
-				"headers": {
-					"x-rapidapi-key": "fc0734c9f9msh31df8e2c2d944ccp100f9bjsn71384937e883",
-					"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-				},
-				"success" : fillAutoComplete2
-			};
+		var searchTerm = document.getElementById("fighter2").value;
+		console.log(searchTerm);
+		const settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q="+ searchTerm +	"&region=US",
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-key": "fc0734c9f9msh31df8e2c2d944ccp100f9bjsn71384937e883",
+				"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+			},
+			"success" : fillAutoComplete2
+		};
 			
-			$.ajax(settings).done(function (response) {
+		$.ajax(settings).done(function (response) {
 			console.log(response);
 			for (var i = 0; i < 6; i++) {
 				nameList2.push(response.quotes[i].shortname + " (" + response.quotes[i].symbol + ")");
 			}
-	});
+		});
 	};
 };
 
