9f515605d0600cef9ba2d640d503f5fe7f5036e8
Matthias <17014025@uhi.ac.uk>
2021-01-24 00:29:45 +0000

    Update query.js

--- htdocs/js/query.js
+++ htdocs/js/query.js
@@ -3,8 +3,20 @@ function init() {
 	document.getElementById("company2").addEventListener("keyup", autocomplete);
 	document.getElementById("btnSubmit").addEventListener("click", compare);
 }
-let company1Info;
-let company2Info;
+const tblfields = [
+	"ab","ae","g",
+	"tp","at","fsl",
+	"cw","sa","cv",
+	"gmo","mc","p",
+	"tc","po"
+];
+const jsonfields = [
+	"alcoholic","adult","gambling",
+	"tobacco","animalTesting","furLeather",
+	"controversialWeapons","smallArms","catholic",
+	"gmo","militaryContract","pesticides",
+	"coal","palmOil"
+];
 function autocomplete() {}
 function compare() {
 	event.preventDefault();
@@ -13,16 +25,22 @@ function compare() {
 	let cp2 = document.getElementById("company2");
 	yahooQuery(cp1.value, 'UK')
 	.then(data => {
-		company1Info = data;
-		console.log(data); // JSON data parsed by `data.json()` call
+		console.log(data.esgScores);
+		fillTable(1,data.esgScores);
 	});
 	yahooQuery(cp2.value, 'UK')
 	.then(data => {
-		company2Info = data;
-		console.log(data); // JSON data parsed by `data.json()` call
+		console.log(data.esgScores);
+		fillTable(2,data.esgScores);
 	});
 }
-
+function fillTable(column,data) {
+	let tbl = [];
+	for(let i in tblfields) {
+		let field = document.getElementById(tblfields[i]+column);
+		field.textContent = data[jsonfields[i]];
+	}
+}
 
 async function yahooQuery(symbol, region = "US", other = {}) {
 	let url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?';
